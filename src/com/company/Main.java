f52a8c7f915b1f1b6130480893dcc14f0bb72d94
fulcrum888 <yaroslav.buneev@gmail.com>
2020-09-14 16:21:39 +0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
